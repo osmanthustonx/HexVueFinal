7a9d03c3389aaf66bda8b54c46d046f3f1c55dfa
BrianKuei <vx3012564897@gmail.com>
2019-10-24 17:49:57 +0800

    vue 初始化

---
+++ config/dev.env.js
@@ -0,0 +1,10 @@
+
+
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"sunbu0331"',
+});
